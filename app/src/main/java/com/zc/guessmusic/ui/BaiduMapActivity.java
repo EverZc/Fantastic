1eda8ee088e8863130200527b08a8bbbd73cad52
Zc <zc_program_private@163.com>
2017-02-22 15:25:29 +0800

    整合百度地图 JAR包 导入完毕

---
+++ app/src/main/java/com/zc/guessmusic/ui/BaiduMapActivity.java
@@ -0,0 +1,15 @@
+package com.zc.guessmusic.ui;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.zc.guessmusic.R;
+
+public class BaiduMapActivity extends AppCompatActivity {
+    public LocationClient
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_baidu_map);
+    }
+}
